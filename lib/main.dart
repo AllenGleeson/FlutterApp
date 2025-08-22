80641fc8cb5a8196a3e2709623b6abf1531414de
AllenGleeson <allen_gleeson@hotmail.com>
2025-08-22 22:32:20 +0100

    Working on routing screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/pages/home.dart';
+import 'package:flutterapp/pages/details.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomePage());
+    return MaterialApp(
+      title: "Flutter Nav Demo",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/details': (context) => const DetailsScreen(),
+      },
+    );
   }
 }
